1223f3e6d3b04f154f8acb2b745b8f0c59b08142
Daniel Reza <daniel.reza1007@gmail.com>
2021-05-04 21:41:18 -0500

    adding views folder

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/public", express.static("./public"));
 }
+
+app.get('/', function (req, res) {
+    res.render('index.html', {});
+  });
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
